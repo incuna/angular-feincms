b54b9238ec7bb58e77ef6ef206b74f700945e9c3
Ming Lee <ming@incuna.com>
2016-07-18 12:23:07 +0100

    Alphabetise modules and add templates.

--- modules/feincms-pages/scripts/init.js
+++ modules/feincms-pages/scripts/init.js
@@ -2,12 +2,13 @@
     'use strict';
 
     angular.module('feincms-pages', [
-        'feincms-pages.pages-detail',
-        'feincms-pages.feincms-page',
-        'feincms-pages.page-group',
-        'feincms-pages.load-page',
         'feincms-pages.constants',
-        'feincms-pages.routes'
+        'feincms-pages.feincms-page',
+        'feincms-pages.load-page',
+        'feincms-pages.page-group',
+        'feincms-pages.pages-detail',
+        'feincms-pages.routes',
+        'feincms-pages.templates'
     ]);
 
 }(window.angular));
